82dfa3151cdf8de6065904327a0821a5adc39d64
Abazceboi <abazceboi@gmail.com>
2026-07-09 01:13:38 +0100

    Enable SQLite WAL mode for high concurrency

--- database.js
+++ database.js
@@ -259,6 +259,7 @@ if (usePostgres) {
                 console.error('Error connecting to SQLite database:', err.message);
             } else {
                 console.log('Connected to the SQLite database.');
+                db.run('PRAGMA journal_mode = WAL;');
                 initializeDatabase();
             }
         });
